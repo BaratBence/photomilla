425fc9ce056e662617555ac6af4d50ad8b15be2a
BaratBence <bencebarat77@gmail.com>
2026-07-19 17:54:34 +0200

    removed unused variable

--- script.js
+++ script.js
@@ -180,8 +180,6 @@ class PhotoGallery {
         let touchStartX = 0;
         let touchStartY = 0;
 
-        let touchStartX = 0;
-
         this.lightbox.addEventListener('touchstart', (e) => {
             console.log("start");
             touchStartX = e.touches[0].clientX;
